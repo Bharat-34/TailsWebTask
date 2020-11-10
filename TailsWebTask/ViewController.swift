4d1b139af49f368751a764d84360d63863d19fb3
Bharat <bharat.gude@twenty70.in>
2020-11-10 19:48:56 +0530

    Initial Commit

---
+++ TailsWebTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TailsWebTask
+//
+//  Created by Bharat on 10/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
