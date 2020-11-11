4bf19dfd578c16d330fe71291f84f280d021a34b
Bharat <bharat.gude@twenty70.in>
2020-11-11 21:59:10 +0530

    required files of pod

---
+++ Pods/Target Support Files/Pods-TailsWebTask-TailsWebTaskUITests/Pods-TailsWebTask-TailsWebTaskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TailsWebTask_TailsWebTaskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TailsWebTask_TailsWebTaskUITests
+@end
---
+++ Pods/Target Support Files/Pods-TailsWebTask/Pods-TailsWebTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TailsWebTask : NSObject
+@end
+@implementation PodsDummy_Pods_TailsWebTask
+@end
